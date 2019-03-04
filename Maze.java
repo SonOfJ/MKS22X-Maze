dde52e0e1edf3042fdc718c30eb468b5e25d067e
Jason Zheng <zzheng3@stuy.edu>
2019-03-04 01:41:51 -0500

    Redid the contructor and toString. Tidied up code and added comments.

--- Maze.java
+++ Maze.java
@@ -2,36 +2,26 @@ import java.util.*;
 import java.io.*;
 public class Maze {
   private char[][] maze;
-  private boolean animate;
+  private boolean animate; //false by default
   public Maze(String filename) throws FileNotFoundException {
-    File text = new File("Maze1.txt");
+    File text = new File(filename);
     Scanner inf = new Scanner(text);
-    int length = inf.nextLine().length();
-    int height = 1;
-    while(inf.hasNextLine()){
+    int height = 0;
+    int length = 0;
+    while (inf.hasNextLine()) { //Builds up the height of the maze.
+      String line = inf.nextLine();
+      length = line.length();
       height = height + 1;
     }
-    maze = new char[height][length];
-    int numE = 0; //Keeps track of the number of Es.
-    int numS = 0; //Keeps track of the number of Ss.
-    for (int i = 0; i < height; i = i + 1) { //Transfers the maze to a 2D array.
+    inf = new Scanner(text);
+    maze = new char[height][length]; //Initialize.
+    int row = 0; //Counter for rows.
+    while (inf.hasNextLine()) { //Add the stuff.
       String line = inf.nextLine();
-      for (int j = 0; j < length; j = j + 1) {
-        maze[i][j] = line.charAt(j);
+      for (int i = 0; i < line.length(); i = i + 1) {
+        maze[row][i] = line.charAt(i);
       }
-    }
-    for (int i = 0; i < maze.length; i = i + 1) { //Seeks for the number of Es and Ss.
-      for (int j = 0; j < maze[0].length; j = j + 1) {
-        if (maze[i][j] == 'E') {
-          numE = numE + 1;
-        }
-        if (maze[i][j] == 'S') {
-          numS = numS + 1;
-        }
-      }
-    }
-    if (numE != 1 || numS != 1) { //If there isn't exactly 1 E and 1 S.
-      throw new IllegalStateException("Invalid maze.");
+      row + row + 1;
     }
     animate = false;
   }
@@ -46,53 +36,16 @@ public class Maze {
     animate = b;
   }
   public void clearTerminal() {
-    //erase terminal, go to top left of screen.
     System.out.println("\033[2J\033[1;1H");
   }
   public String toString() {
     String display = "";
     for (int i = 0; i < maze.length; i = i + 1) {
-      for (int j = 0; j < maze[0].length; j = j + 1) {
+      for (int j = 0; j < maze[i].length; j = j + 1) {
         display = display + maze[i][j];
       }
       display = display + "\n";
     }
     return display;
   }
-  public int solve() {
-    for ()
-            //find the location of the S.
-
-            //erase the S
-
-            //and start solving at the location of the s.
-            //return solve(???,???);
-    }
-
-    /*
-      Recursive Solve function:
-
-      A solved maze has a path marked with '@' from S to E.
-
-      Returns the number of @ symbols from S to E when the maze is solved,
-      Returns -1 when the maze has no solution.
-
-      Postcondition:
-        The S is replaced with '@' but the 'E' is not.
-        All visited spots that were not part of the solution are changed to '.'
-        All visited spots that are part of the solution are changed to '@'
-    */
-    private int solve(int row, int col){ //you can add more parameters since this is private
-
-        //automatic animation! You are welcome.
-        if(animate){
-            clearTerminal();
-            System.out.println(this);
-            wait(20);
-        }
-
-        //COMPLETE SOLVE
-        return -1; //so it compiles
-    }
-
 }
